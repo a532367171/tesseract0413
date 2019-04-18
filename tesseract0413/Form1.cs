afea562c188b056c1da1b1c352567f796ee40969
a532367171 <532367171@.qq.com>
2019-04-18 21:31:47 +0800

    0418

--- tesseract0413/Form1.cs
+++ tesseract0413/Form1.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Windows.Forms;
 using O2S.Components.PDFRender4NET;
 using Tesseract;
+using tesseract0413.Model;
 
 namespace tesseract0413
 {
@@ -15,6 +16,7 @@ namespace tesseract0413
 
         List<List<Bitmap>> xList1 = new List<List<Bitmap>>();
 
+        private List<Confirm_the_table_info> _List_Confirm_the_table_info = new List<Confirm_the_table_info>();
         public Form1()
         {
             InitializeComponent();
@@ -49,25 +51,141 @@ namespace tesseract0413
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 //ConvertPDF2Image(openFileDialog1.FileName, "D:\\", "A", 1, 5, System.Drawing.Imaging.ImageFormat.Jpeg,Definition.Five);
-                xList1= ConvertPDF2ListImage(openFileDialog1.FileName, "D:\\", "A", 1, 5, System.Drawing.Imaging.ImageFormat.Jpeg, Definition.Five);
+                xList1 = ConvertPDF2ListImage(openFileDialog1.FileName, "D:\\", "A", 1,10, System.Drawing.Imaging.ImageFormat.Jpeg, Definition.Five);
             }
 
+            foreach (var VARIABLE in xList1)
+            {
+                Confirm_the_table_info _Confirm_the_table_info = new Confirm_the_table_info();
 
+
+                using (var ocr0 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+                    var page0 = ocr0.Process(VARIABLE[0]);
+
+                    _Confirm_the_table_info.shebeimingcheng = page0.GetText();
+                }
+
+
+
+
+
+                using (var ocr1 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+
+                    ocr1.SetVariable("tessedit_char_whitelist",
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.");
+
+                    var page1 = ocr1.Process(VARIABLE[1]);
+
+                    _Confirm_the_table_info.xinhaoguige = page1.GetText();
+
+                }
+
+
+
+
+
+                using (var ocr2 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+
+                    ocr2.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.");
+
+                    var page2 = ocr2.Process(VARIABLE[2]);
+
+                    _Confirm_the_table_info.xinhaoguige = page2.GetText();
+
+
+                }
+
+
+
+
+
+                using (var ocr3 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+
+                    ocr3.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.");
+
+                    var page3 = ocr3.Process(VARIABLE[3]);
+
+                    _Confirm_the_table_info.guanlibianhao = page3.GetText();
+
+
+                }
+
+
+
+
+
+                using (var ocr4 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+
+
+                    var page4 = ocr4.Process(VARIABLE[4]);
+
+                    _Confirm_the_table_info.querenren = page4.GetText();
+
+
+                }
+
+
+
+
+
+                using (var ocr5 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+
+                    ocr5.SetVariable("tessedit_char_whitelist", "年月日0123456789-.");
+
+                    var page5 = ocr5.Process(VARIABLE[5]);
+
+                    _Confirm_the_table_info.guanlibianhao = page5.GetText();
+
+
+                }
+
+
+
+
+
+                using (var ocr6 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+
+                    ocr6.SetVariable("tessedit_char_whitelist", "福建省恒准检测科技有限公司建筑科学研究院()");
+
+                    var page6 = ocr6.Process(VARIABLE[6]);
+
+                    _Confirm_the_table_info.jiandingdanwei = page6.GetText();
+
+                }
+
+
+
+
+
+                using (var ocr7 = new TesseractEngine("./tessdata", "chi_sim", EngineMode.TesseractOnly))
+                {
+
+                    ocr7.SetVariable("tessedit_char_whitelist",
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.");
+
+                    var page7 = ocr7.Process(VARIABLE[7]);
+
+                    _Confirm_the_table_info.guanlibianhao = page7.GetText();
+                }
+
+                _List_Confirm_the_table_info.Add(_Confirm_the_table_info);
+
+            }
+
+            var SSSS = 1;
         }
 
 
 
 
 
-
-
-        public enum Definition
-
-        {
-
-            One = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10
-
-        }
 
         /// <summary>
 
@@ -151,7 +269,7 @@ namespace tesseract0413
 
         }
 
-        public  List<List<Bitmap>> ConvertPDF2ListImage(string pdfInputPath, string imageOutputPath, string imageName, int startPageNum, int endPageNum, System.Drawing.Imaging.ImageFormat imageFormat, Definition definition)
+        public List<List<Bitmap>> ConvertPDF2ListImage(string pdfInputPath, string imageOutputPath, string imageName, int startPageNum, int endPageNum, System.Drawing.Imaging.ImageFormat imageFormat, Definition definition)
         {
 
             List<List<Bitmap>> xList = new List<List<Bitmap>>();
@@ -218,8 +336,20 @@ namespace tesseract0413
         }
 
 
+    }
+
+
+
+
+    public enum Definition
+    {
+
+        One = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10
 
     }
+
+
+
 }
 
 
---
+++ tesseract0413/Model/Confirm_the_table_info.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tesseract0413.Model
+{
+    public class Confirm_the_table_info
+    {
+        /// <summary>
+        /// shebeimingcheng
+        /// </summary>		
+        private string _shebeimingcheng;
+        public string shebeimingcheng
+        {
+            get { return _shebeimingcheng; }
+            set { _shebeimingcheng = value; }
+        }
+        /// <summary>
+        /// xinhaoguige
+        /// </summary>		
+        private string _xinhaoguige;
+        public string xinhaoguige
+        {
+            get { return _xinhaoguige; }
+            set { _xinhaoguige = value; }
+        }
+        /// <summary>
+        /// chuchangbianhao
+        /// </summary>		
+        private string _chuchangbianhao;
+        public string chuchangbianhao
+        {
+            get { return _chuchangbianhao; }
+            set { _chuchangbianhao = value; }
+        }
+        /// <summary>
+        /// guanlibianhao
+        /// </summary>		
+        private string _guanlibianhao;
+        public string guanlibianhao
+        {
+            get { return _guanlibianhao; }
+            set { _guanlibianhao = value; }
+        }
+        /// <summary>
+        /// querenren
+        /// </summary>		
+        private string _querenren;
+        public string querenren
+        {
+            get { return _querenren; }
+            set { _querenren = value; }
+        }
+        /// <summary>
+        /// riqi
+        /// </summary>		
+        private string _riqi;
+        public string riqi
+        {
+            get { return _riqi; }
+            set { _riqi = value; }
+        }
+        /// <summary>
+        /// jiandingdanwei
+        /// </summary>		
+        private string _jiandingdanwei;
+        public string jiandingdanwei
+        {
+            get { return _jiandingdanwei; }
+            set { _jiandingdanwei = value; }
+        }
+        /// <summary>
+        /// zhengshubianhao
+        /// </summary>		
+        private string _zhengshubianhao;
+        public string zhengshubianhao
+        {
+            get { return _zhengshubianhao; }
+            set { _zhengshubianhao = value; }
+        }
+    }
+}
